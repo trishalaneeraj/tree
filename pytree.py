db90e8683a6e510310168b6aab474b6c94a3f108
trishalaneeraj <trishalaneeraj@gmail.com>
2016-10-02 15:58:51 -0400

    code climate fixes

--- pytree.py
+++ pytree.py
@@ -14,6 +14,7 @@ def print_tree(path, indent="", lst=[]):
     files = os.listdir(path)
     files = list(filter(lambda f: not f.startswith('.'), files))
     files = sorted(files, key=sortkeys)
+    # files = sorted(files)
 
     for i in range(0, len(files)):
         fullpath = path + "/" + files[i]
@@ -23,6 +24,7 @@ def print_tree(path, indent="", lst=[]):
 
         else:
             print(indent + '├── ' + files[i])
+
         if os.path.isdir(fullpath):
             if (i == len(files) - 1):
                 print_tree(fullpath, indent + '    ')
@@ -31,26 +33,26 @@ def print_tree(path, indent="", lst=[]):
     return lst
 
 if __name__ == '__main__':
-	if len(sys.argv) == 1:
-		dir = '.'
-	if len(sys.argv) == 2:
-	    dir = sys.argv[1]
-	elif len(sys.argv) > 2:
-	    print("Usage: %s [path]" % sys.argv[0])
-	    sys.exit(0)
+    if len(sys.argv) == 1:
+        dir = '.'
+    if len(sys.argv) == 2:
+        dir = sys.argv[1]
+    elif len(sys.argv) > 2:
+        print("Usage: %s [path]" % sys.argv[0])
+        sys.exit(0)
 
-	if not os.path.isdir(dir):
-	    print("E: that is not a valid path")
-	    sys.exit(0)
+    if not os.path.isdir(dir):
+        print("E: that is not a valid path")
+        sys.exit(0)
 
-	print(dir)
-	k = print_tree(dir)
-	print
+    print(dir)
+    k = print_tree(dir)
+    print
 
-	d1, f1 = 0, 0
-	for i in k:
-		if os.path.isdir(i):
-			d1 += 1
-		else:
-			f1 += 1
-	print("\n" + str(d1) + " directories, " + str(f1) + " files") #, k, "list"
+    d1, f1 = 0, 0
+    for i in k:
+        if os.path.isdir(i):
+            d1 += 1
+        else:
+            f1 += 1
+    print("\n" + str(d1) + " directories, " + str(f1) + " files")  # , k, "list"
