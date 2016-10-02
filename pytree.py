6f347081fa2d83c926e9969d2477db17c5cf8445
trishalaneeraj <trishalaneeraj@gmail.com>
2016-10-02 14:02:34 -0400

    removed files6

--- pytree.py
+++ pytree.py
@@ -10,34 +10,30 @@ def sortkeys(s):
     return re.sub('[^A-Za-z0-9]+', '', s).lower()
 
 
-def print_tree(path, indent="", lst = []):
+def print_tree(path, indent="", lst=[]):
     files = os.listdir(path)
-    files = list(filter( lambda f: not f.startswith('.'), files))
+    files = list(filter(lambda f: not f.startswith('.'), files))
     files = sorted(files, key=sortkeys)
 
     for i in range(0, len(files)):
         fullpath = path + "/" + files[i]
         lst.append(fullpath)
-        if i == len(files)-1:
+        if i == len(files) - 1:
             print(indent + '└── ' + files[i])
-
         else:
             print(indent + '├── ' + files[i])
 
 
         if os.path.isdir(fullpath):
-            if i == len(files)-1:
-                print_tree(fullpath, indent+'    ')
+            if i == len(files) - 1:
+                print_tree(fullpath, indent + '    ')
             else:
-                print_tree(fullpath, indent+'│   ')
+                print_tree(fullpath, indent + '│   ')
     return lst
 
-
-
 if __name__ == '__main__':
-
-	if len(sys.argv) == 1:
-		dir = '.'
+    if len(sys.argv) == 1:
+        dir = '.'
 	if len(sys.argv) == 2:
 	    dir = sys.argv[1]
 	elif len(sys.argv) > 2:
